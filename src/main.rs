afdc6142b606a1926c59e8278bccd150280a6b9f
John Bretz <johnfish218@gmail.com>
2020-01-25 12:33:25 -0600

    peripheral layout done

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,89 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
                          // extern crate panic_itm; // logs messages over ITM; requires ITM support
                          // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
+use core::mem::replace;
+use core::ptr;
 use cortex_m_rt as rt;
 
 pub const FREQ: u32 = 16_000_000;
 
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct SysTick {
-    ctrl: u32,
-    load: u32,
-    val: u32,
-    calib: u32,
+struct Peripherals {
+    systick: Option<SysTick>,
 }
+impl Peripherals {
+    fn take_systick(&mut self) -> SysTick {
+        let p = replace(&mut self.systick, None);
+        p.unwrap()
+    }
+}
+static mut PERIPHERALS: Peripherals = Peripherals {
+    systick: Some(SysTick),
+};
+
+#[repr(C)]
+pub struct SysTick;
 
 impl SysTick {
-    pub fn get() -> SysTick {
-        unsafe { *(0xE000_E010 as *const SysTick) }
+    const CTRL: *mut u32 = 0xE000_E010 as *mut u32;
+    const LOAD: *mut u32 = 0xE000_E014 as *mut u32;
+    const VAL: *mut u32 = 0xE000_E018 as *mut u32;
+    const CALIB: *mut u32 = 0xE000_E01C as *mut u32;
+
+    #[inline(always)]
+    unsafe fn disable(&self) {
+        ptr::write(Self::CTRL, 0);
+    }
+
+    #[inline(always)]
+    unsafe fn enable(&self) {
+        ptr::write(Self::CTRL, 1);
+    }
+
+    #[inline(always)]
+    unsafe fn set_load(&self, val: u32) {
+        ptr::write(Self::LOAD, val);
+    }
+
+    #[inline(always)]
+    unsafe fn get_count_flag(&self) -> bool {
+        let reg = (ptr::read(Self::CTRL) >> 16) & 0b1;
+        if reg == 1 {
+            return true;
+        }
+        false
+    }
+
+    pub fn delay_ms(&self, delay: u32) {
+        unsafe {
+            self.disable();
+            self.set_load(delay * (FREQ / 8000));
+            self.enable();
+
+            while !self.get_count_flag() {}
+
+            self.disable();
+        }
+    }
+
+    pub fn delay_us(&self, delay: u32) {}
+}
+
+impl Drop for SysTick {
+    fn drop(&mut self) {
+        unsafe { PERIPHERALS.systick = Some(SysTick) }
     }
 }
 
 #[rt::entry]
 fn main() -> ! {
     unsafe {
-        let a = SysTick::get();
-        let b = SysTick::get();
+        {
+            let a: SysTick = PERIPHERALS.take_systick();
+            a.delay_ms(5000);
+        }
+        let b = PERIPHERALS.take_systick();
+        core::mem::drop(b);
+        let c = PERIPHERALS.take_systick();
         loop {}
     }
 }
