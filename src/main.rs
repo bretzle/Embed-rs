60e840e2755e0316c9ca56f70b85ed38319ddd0d
John Bretz <johnfish218@gmail.com>
2020-01-26 11:30:02 -0600

    User can only modify the ODR through the BSRR

--- src/main.rs
+++ src/main.rs
@@ -41,13 +41,10 @@ unsafe fn init_leds() {
     gpio.set_moder(0x55155400);
 }
 
-unsafe fn light_led(num: u32) {
+unsafe fn light_led(num: u16) {
     let mut gpio: GPIOB = PERIPHERALS.take_gpiob();
+    const clear_mask: u16 = 0xF7E0;
+    let set_mask = ((num & 0x3F) << 5) | ((num & 0x3C0) << 6);
 
-    gpio.clear_odr();
-
-    let a = (num & 0x3F) << 5;
-    let b = (num & 0x3C0) << 6;
-
-    gpio.set_odr(a | b);
+    gpio.set_clear_odr(clear_mask, set_mask);
 }
--- stmlib/src/gpio/gpiob.rs
+++ stmlib/src/gpio/gpiob.rs
@@ -8,18 +8,29 @@ impl GPIOB {
     const MODER: *mut u32 = 0x4002_0400 as *mut u32;
     const IDR: *mut u32 = 0x4002_0410 as *mut u32;
     const ODR: *mut u32 = 0x4002_0414 as *mut u32;
+    const BSRR: *mut u32 = 0x4002_0418 as *mut u32;
 
     pub unsafe fn set_moder(&mut self, mask: u32) {
         let val = ptr::read(Self::MODER) | mask;
         ptr::write(Self::MODER, val);
     }
 
-    pub unsafe fn clear_odr(&mut self) {
-        ptr::write(Self::ODR, 0);
+    pub unsafe fn read_odr(&self) -> u32 {
+        ptr::read(Self::ODR)
     }
 
-    pub unsafe fn set_odr(&mut self, val: u32) {
-        ptr::write(Self::ODR, val);
+    pub unsafe fn clear_odr_bits(&mut self, val: u16) {
+        let mask = (val as u32) << 16;
+        ptr::write(Self::BSRR, mask);
+    }
+
+    pub unsafe fn set_odr_bits(&mut self, val: u16) {
+        ptr::write(Self::BSRR, val as u32);
+    }
+
+    pub unsafe fn set_clear_odr(&mut self, clear: u16, set: u16) {
+        let mask = ((clear as u32) << 16) | set as u32;
+        ptr::write(Self::BSRR, mask);
     }
 }
 
